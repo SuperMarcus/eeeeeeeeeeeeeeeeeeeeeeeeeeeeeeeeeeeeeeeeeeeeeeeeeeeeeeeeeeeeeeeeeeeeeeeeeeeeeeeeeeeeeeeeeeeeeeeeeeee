550684c2bdc72dbfe00ceae57ce3cefe02e14ec3
Marcus Zhou <7273813+SuperMarcus@users.noreply.github.com>
2018-12-25 09:58:26 -0500

    Better eEe.swift
    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ eEe.swift
@@ -0,0 +1,14 @@
+/// ---- EeEeEeEe
+
+typealias eeeee = String
+let eee = { (eeeeee: eeeee) -> () in print(eeeeee) }
+let e: eeeee = "e"
+func ee(_ eeee: () -> ()){ while true { eeee() } }
+
+/// ---- EeEeEeEe
+
+/// ++++ EeEeEeEe
+
+ee { eee(e) }
+
+/// ++++ EeEeEeEe
